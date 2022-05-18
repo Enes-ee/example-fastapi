0ca0c78124a5f3a7499af953e6b14d3d93bde4ae
Enes Erden <aokiji.ee@hotmail.com>
2022-05-18 18:13:13 +0300

    inital commit

---
+++ app/main.py
@@ -0,0 +1,32 @@
+from fastapi import Body, FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from . import models
+from .database import engine
+from .schemas import *
+from .routers import post, user, auth, vote
+from .config import settings
+
+# models.Base.metadata.create_all(bind=engine)
+
+origins = ["https://www.google.com"]
+
+app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+app.include_router(post.router)
+app.include_router(user.router)
+app.include_router(auth.router)
+app.include_router(vote.router)
+
+# This fragment of code is not required. It is placed just for informational purposes.
+@app.get("/")
+def root():
+    return {"message" : "root directory"}
+
+
